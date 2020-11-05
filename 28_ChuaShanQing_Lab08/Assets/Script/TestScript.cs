e46c89510053ebe54192f1f2048bea656ea37e17
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2020-11-05 21:03:24 +0800

    Lab 8 done

---
+++ 28_ChuaShanQing_Lab08/Assets/Script/TestScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour
+{
+    public GameObject scoreText;
+    float SpaceCounter = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.GetComponent<Text>().text = "Start Function";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpaceCounter++;
+            Debug.Log("SpaceBarCounter:" +SpaceCounter);
+        }
+        scoreText.GetComponent<Text>().text = "Spacebar: " + SpaceCounter.ToString();
+    }
+}
