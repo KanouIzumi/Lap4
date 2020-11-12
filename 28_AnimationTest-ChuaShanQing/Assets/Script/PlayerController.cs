6c07933e1dcbd3c5e4732c0c8b158af7752eb1de
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2020-11-12 14:43:31 +0800

    Can kill enemy

--- 28_AnimationTest-ChuaShanQing/Assets/Script/PlayerController.cs
+++ 28_AnimationTest-ChuaShanQing/Assets/Script/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     Animator playerAnim;
     public float speed = 1.5f;
+    bool collide;
+    int enemyHP = 0;
+    public GameObject Enemy;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +19,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W)) //go forward
         {
             transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
             playerAnim.SetBool("IsStrafe", true);
         }
         else if (Input.GetKeyUp(KeyCode.W))
@@ -30,11 +35,53 @@ public class PlayerController : MonoBehaviour
             playerAnim.SetTrigger("TrigAttack");
         }
 
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (Input.GetKey(KeyCode.S)) // go backward
         {
-            playerAnim.SetTrigger("TrigAttack");
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            playerAnim.SetBool("IsStrafe", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            playerAnim.SetBool("IsStrafe", false);
         }
 
+        if (Input.GetKey(KeyCode.A)) // go left
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+            playerAnim.SetBool("IsStrafe", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            playerAnim.SetBool("IsStrafe", false);
+        }
+
+        if (Input.GetKey(KeyCode.D)) // go right
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+            playerAnim.SetBool("IsStrafe", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            playerAnim.SetBool("IsStrafe", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            playerAnim.SetTrigger("TrigAttack");
+            enemyHP++;
+
+            if (collide == true)
+            {
+                if (enemyHP == 5)
+                {
+                    Destroy(Enemy);
+                }
+            }
+        }
 
 
     }
@@ -47,5 +94,23 @@ public class PlayerController : MonoBehaviour
         }
 
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            collide = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            collide = false;
+        }
+    }
+
+
 }
 
