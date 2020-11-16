700d93c1381c63d7947dc7244cbd4cae40bd1050
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2020-11-16 15:18:44 +0800

    When Died will not move

--- AnimeDemo/Assets/Script/Playercontroller.cs
+++ AnimeDemo/Assets/Script/Playercontroller.cs
@@ -7,7 +7,7 @@ public class Playercontroller : MonoBehaviour
 {
     public float speed = 5f;
     public float jumpForce = 5f;
-    float HP = 10f;
+    float health = 10f;
     public GameObject healthPointText;
 
     public Animator playerAnim;
@@ -19,78 +19,84 @@ public class Playercontroller : MonoBehaviour
     void Start()
     {
         healthPointText.GetComponent<Text>().text = "Start Function";
-        healthPointText.GetComponent<Text>().text = "Health Points: " + HP.ToString();
+        healthPointText.GetComponent<Text>().text = "Health Points: " + health.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W)) //go forward
+        if (health > 0)
         {
-            transform.Translate(Vector3.forward * Time.deltaTime * speed);
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-            playerAnim.SetBool("IsMove", true);
+            if (Input.GetKey(KeyCode.W)) //go forward
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                playerAnim.SetBool("IsMove", true);
+            }
+            else if (Input.GetKeyUp(KeyCode.W))
+            {
+                playerAnim.SetBool("IsMove", false);
+            }
+
+            if (Input.GetKey(KeyCode.S)) // go backward
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+                playerAnim.SetBool("IsMove", true);
+            }
+            else if (Input.GetKeyUp(KeyCode.S))
+            {
+                playerAnim.SetBool("IsMove", false);
+            }
+
+            if (Input.GetKey(KeyCode.A)) // go left
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                transform.rotation = Quaternion.Euler(0, -90, 0);
+                playerAnim.SetBool("IsMove", true);
+            }
+            else if (Input.GetKeyUp(KeyCode.A))
+            {
+                playerAnim.SetBool("IsMove", false);
+            }
+
+            if (Input.GetKey(KeyCode.D)) // go right
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                transform.rotation = Quaternion.Euler(0, 90, 0);
+                playerAnim.SetBool("IsMove", true);
+            }
+            else if (Input.GetKeyUp(KeyCode.D))
+            {
+                playerAnim.SetBool("IsMove", false);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space) && IsOnPlane)
+            {
+                playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                playerAnim.SetTrigger("TrigFlip");
+                IsOnPlane = false;
+            }
+
         }
-        else if (Input.GetKeyUp(KeyCode.W))
+        else
         {
             playerAnim.SetBool("IsMove", false);
         }
-
-        if (Input.GetKey(KeyCode.S)) // go backward
-        {
-            transform.Translate(Vector3.forward * Time.deltaTime * speed);
-            transform.rotation = Quaternion.Euler(0, 180, 0);
-            playerAnim.SetBool("IsMove", true);
-        }
-        else if (Input.GetKeyUp(KeyCode.S))
-        {
-            playerAnim.SetBool("IsMove", false);
-        }
-
-        if (Input.GetKey(KeyCode.A)) // go left
-        {
-            transform.Translate(Vector3.forward * Time.deltaTime * speed);
-            transform.rotation = Quaternion.Euler(0, -90, 0);
-            playerAnim.SetBool("IsMove", true);
-        }
-        else if (Input.GetKeyUp(KeyCode.A))
-        {
-            playerAnim.SetBool("IsMove", false);
-        }
-
-        if (Input.GetKey(KeyCode.D)) // go right
-        {
-            transform.Translate(Vector3.forward * Time.deltaTime * speed);
-            transform.rotation = Quaternion.Euler(0, 90, 0);
-            playerAnim.SetBool("IsMove", true);
-        }
-        else if (Input.GetKeyUp(KeyCode.D))
-        {
-            playerAnim.SetBool("IsMove", false);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Space) && IsOnPlane)
-        {
-            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            playerAnim.SetTrigger("TrigFlip");
-            IsOnPlane = false;
-        }
+    
 
 
         if (Input.GetKeyDown(KeyCode.K))
         {
-          HP--;
-          healthPointText.GetComponent<Text>().text = "Health Points: " + HP.ToString();
-            if (HP == 0)
+          health--;
+          healthPointText.GetComponent<Text>().text = "Health Points: " + health.ToString();
+            if (health == 0)
             {
                 playerAnim.SetTrigger("TrigDeath");
                 speed = 0;
                 jumpForce = 0;
             }
         }
-
-
-
     }
 
 
