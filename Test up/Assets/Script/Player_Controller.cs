3d5998288abd505ed19653bda46e6b3db0101f51
KanouIzumi <72852474+KanouIzumi@users.noreply.github.com>
2020-11-26 14:38:47 +0800

    testing

---
+++ Test up/Assets/Script/Player_Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player_Controller : MonoBehaviour
+{
+    public GameObject TimerText;
+    public Animator PlayerAnim;
+    public Rigidbody playerRb;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
